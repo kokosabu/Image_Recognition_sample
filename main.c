8deab1f698ff365f0a8053e9f7e952047c9466a2
MIURA Yasuyuki <kokosabu@gmail.com>
2018-01-04 16:35:13 +0900

    Implement BITMAPINFOHEADER

--- main.c
+++ main.c
@@ -27,11 +27,9 @@ int main(int argc, char *argv[])
 {
     FILE *input;
     FILE *output;
-    uint16_t bfType;
-    uint32_t bfSize;
-    uint16_t bfReserved1;
-    uint16_t bfReserved2;
-    uint32_t bfOffBits;
+    BITMAPFILEHEADER file_header;
+    BITMAPINFOHEADER info_header;
+    uint32_t size;
     uint8_t t;
 
     if(argc <= 1) {
@@ -51,27 +49,68 @@ int main(int argc, char *argv[])
     printf("%c\n", t); /* M */
 #endif
 
-    fread(&bfType, 2, 1, input);
-    printf("%c %c\n", (bfType >> 8)&0xFF, (bfType)&0xFF); /* M B */
-    fread(&bfSize, 4, 1, input);
-    printf("%d\n", bfSize);
-    fread(&bfReserved1, 2, 1, input);
-    printf("%d\n", bfReserved1);
-    fread(&bfReserved2, 2, 1, input);
-    printf("%d\n", bfReserved2);
-    fread(&bfOffBits, 4, 1, input);
-    printf("%d\n", bfOffBits);
+    fread(&file_header.bfType, 2, 1, input);
+    printf("bfType:%c%c\n", (file_header.bfType >> 8)&0xFF, (file_header.bfType)&0xFF); /* M B */
+    fread(&file_header.bfSize, 4, 1, input);
+    printf("bfSize:%d\n", file_header.bfSize);
+    fread(&file_header.bfReserved1, 2, 1, input);
+    fread(&file_header.bfReserved2, 2, 1, input);
+    fread(&file_header.bfOffBits, 4, 1, input);
+    printf("bfOffBits:%d\n", file_header.bfOffBits);
+
+    fread(&size, 4, 1, input);
+    printf("Size:%d\n", size);
+    if(size == 40) {
+        info_header.biSize = size;
+
+        fread(&info_header.biWidth, 4, 1, input);
+        printf("biWidth:%d\n", info_header.biWidth);
+        fread(&info_header.biHeight, 4, 1, input);
+        printf("biHeight:%d\n", info_header.biHeight);
+        fread(&info_header.biPlanes, 2, 1, input);
+        printf("biPlanes:%d\n", info_header.biPlanes);
+        fread(&info_header.biBitCount, 2, 1, input);
+        printf("biBitCount:%d\n", info_header.biBitCount);
+        fread(&info_header.biCompression, 4, 1, input);
+        printf("biCompression:%d\n", info_header.biCompression);
+        fread(&info_header.biSizeImage, 4, 1, input);
+        printf("biSizeImage:%d\n", info_header.biSizeImage);
+        fread(&info_header.biXPelsPerMeter, 4, 1, input);
+        printf("biXPelsPerMeter:%d\n", info_header.biXPelsPerMeter);
+        fread(&info_header.biYPelsPerMeter, 4, 1, input);
+        printf("biYPelsPerMeter:%d\n", info_header.biYPelsPerMeter);
+        fread(&info_header.biClrUsed, 4, 1, input);
+        printf("biClrUsed:%d\n", info_header.biClrUsed);
+        fread(&info_header.biClrImportant, 4, 1, input);
+        printf("biClrImportant:%d\n", info_header.biClrImportant);
+
+    } else {
+        printf("Not supported file header\n");
+        return 0;
+    }
 
     output = fopen("test", "wb");
     if(output == NULL) {
         return 0;
     }
 
-    fwrite(&bfType, 2, 1, output);
-    fwrite(&bfSize, 4, 1, output);
-    fwrite(&bfReserved1, 2, 1, output);
-    fwrite(&bfReserved2, 2, 1, output);
-    fwrite(&bfOffBits, 4, 1, output);
+    fwrite(&file_header.bfType, 2, 1, output);
+    fwrite(&file_header.bfSize, 4, 1, output);
+    fwrite(&file_header.bfReserved1, 2, 1, output);
+    fwrite(&file_header.bfReserved2, 2, 1, output);
+    fwrite(&file_header.bfOffBits, 4, 1, output);
+
+    fwrite(&info_header.biSize, 4, 1, output);
+    fwrite(&info_header.biWidth, 4, 1, output);
+    fwrite(&info_header.biHeight, 4, 1, output);
+    fwrite(&info_header.biPlanes, 2, 1, output);
+    fwrite(&info_header.biBitCount, 2, 1, output);
+    fwrite(&info_header.biCompression, 4, 1, output);
+    fwrite(&info_header.biSizeImage, 4, 1, output);
+    fwrite(&info_header.biXPelsPerMeter, 4, 1, output);
+    fwrite(&info_header.biYPelsPerMeter, 4, 1, output);
+    fwrite(&info_header.biClrUsed, 4, 1, output);
+    fwrite(&info_header.biClrImportant, 4, 1, output);
 
     fclose(input);
     fclose(output);
