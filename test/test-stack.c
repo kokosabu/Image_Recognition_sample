df0ced17a2e15b80288e05929f4f27a5c04f9cda
MIURA Yasuyuki <kokosabu@gmail.com>
2018-06-04 23:57:05 +0900

    [WIP] cutter tutorial

--- test/test-stack.c
+++ test/test-stack.c
@@ -1,10 +1,37 @@
 #include <cutter.h>
 #include <stack.h>
 
+void test_new_stack (void);
+void test_push (void);
+
+static Stack *stack;
+
+void
+cut_setup (void)
+{
+    stack = NULL;
+}
+
+void
+cut_teardown (void)
+{
+    if (stack)
+        stack_free(stack);
+}
+
 void
 test_new_stack (void)
 {
-    Stack *stack;
     stack = stack_new();
     cut_assert(stack_is_empty(stack));
+}
+
+void
+test_push (void)
+{
+    stack = stack_new();
+    cut_assert_equal_int(0, stack_get_size(stack));
+    stack_push(stack, 100);
+    cut_assert_equal_int(1, stack_get_size(stack));
+    cut_assert(!stack_is_empty(stack));
 }
