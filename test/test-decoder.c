6ed88ab423738841afaebe313e6e09bf72597a25
MIURA Yasuyuki <kokosabu@gmail.com>
2018-06-11 23:22:33 +0900

    update test code

--- test/test-decoder.c
+++ test/test-decoder.c
@@ -15,14 +15,38 @@ void cut_teardown (void)
 
 void test_one_bit_read(void)
 {
-    int a;
+    int bit;
     uint8_t input[1];
     int byte_pos;
     int bit_pos;
+    int i;
 
     input[0] = 0xFF;
     byte_pos = 0;
     bit_pos = 0;
-    a = one_bit_read(input, &byte_pos, &bit_pos);
-    cut_assert(a == 1);
+    for(i = 0; i < 8; i++) {
+        cut_assert(byte_pos == 0);
+        cut_assert(bit_pos == i);
+        bit = one_bit_read(input, &byte_pos, &bit_pos);
+        cut_assert(bit == 1);
+    }
+    cut_assert(byte_pos == 1);
+    cut_assert(bit_pos == 0);
+
+    input[0] = 0x55;
+    byte_pos = 0;
+    bit_pos = 0;
+    for(i = 0; i < 4; i++) {
+        cut_assert(byte_pos == 0);
+        cut_assert(bit_pos == i*2);
+        bit = one_bit_read(input, &byte_pos, &bit_pos);
+        cut_assert(bit == 1);
+
+        cut_assert(byte_pos == 0);
+        cut_assert(bit_pos == i*2+1);
+        bit = one_bit_read(input, &byte_pos, &bit_pos);
+        cut_assert(bit == 0);
+    }
+    cut_assert(byte_pos == 1);
+    cut_assert(bit_pos == 0);
 }
