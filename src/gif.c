2c5724d9ea6f4e9fdad9002257ef303d4718a88a
MIURA Yasuyuki <kokosabu@gmail.com>
2018-06-19 23:43:46 +0900

    implement Block: Image Block, Graphic Control Extension

--- src/gif.c
+++ src/gif.c
@@ -21,10 +21,15 @@ void decode_gif(FILE *input, IMAGEINFO *image_info, RGBTRIPLE ***image_data)
     uint16_t image_height;
     uint8_t local_color_table_flag;
     uint8_t interlace_flag;
-    uint8_t soft_flag;
+    uint8_t local_sort_flag;
     uint8_t reserved;
     uint8_t size_of_local_color_table;
+    uint8_t LZW_minimum_code_side;
+    uint8_t block_size;
+    uint8_t block_image_data[255];
+    uint8_t block_terminator;
     RGBTRIPLE *global_color_table;
+    RGBTRIPLE *local_color_table;
     int i;
 
     fread(signature, 1, 3, input);
@@ -73,19 +78,57 @@ void decode_gif(FILE *input, IMAGEINFO *image_info, RGBTRIPLE ***image_data)
         fread(&byte, 1, 1, input);
         image_left_position += ((unsigned int)byte) << 8;
 
-        // uint16_t image_top_position;
-        // uint16_t image_width;
-        // uint16_t image_height;
-        // uint8_t local_color_table_flag;
-        // uint8_t interlace_flag;
-        // uint8_t soft_flag;
-        // uint8_t reserved;
-        // uint8_t size_of_local_color_table;
+        fread(&byte, 1, 1, input);
+        image_top_position = byte;
+        fread(&byte, 1, 1, input);
+        image_top_position += ((unsigned int)byte) << 8;
+
+        fread(&byte, 1, 1, input);
+        image_width = byte;
+        fread(&byte, 1, 1, input);
+        image_width += ((unsigned int)byte) << 8;
+
+        fread(&byte, 1, 1, input);
+        image_height = byte;
+        fread(&byte, 1, 1, input);
+        image_height += ((unsigned int)byte) << 8;
+
+        fread(&byte, 1, 1, input);
+        local_color_table_flag    = (byte & 0x80) >> 7;
+        interlace_flag            = (byte & 0x40) >> 6;
+        local_sort_flag           = (byte & 0x20) >> 5;
+        reserved                  = (byte & 0x18) >> 3;
+        size_of_local_color_table = (byte & 0x07);
+
+        if(local_color_table_flag == 1) {
+            local_color_table  = (RGBTRIPLE *)malloc(sizeof(RGBTRIPLE) * pow(2, size_of_local_color_table));
+            for(i = 0; i < pow(2, size_of_local_color_table); i++) {
+                fread(&((local_color_table[i]).rgbtRed),   1, 1, input);
+                fread(&((local_color_table[i]).rgbtGreen), 1, 1, input);
+                fread(&((local_color_table[i]).rgbtBlue),  1, 1, input);
+            }
+        } else {
+            local_color_table = NULL;
+        }
+        fread(&LZW_minimum_code_side, 1, 1, input);
+        fread(&block_size, 1, 1, input);
+        fread(block_image_data, 1, block_size, input);
+        fread(&block_terminator, 1, 1, input);
     } else if(byte == 0x21) {
-        /* Graphic Control Extension */
-        /* Comment Extension */
-        /* Plain Text Extension */
-        /* Application Extension */
+        fread(&byte, 1, 1, input);
+
+        if(byte == 0xf9) {
+            /* Graphic Control Extension */
+            fread(&byte, 1, 1, input);
+            if(byte != 0x04) {
+                printf("error\n");
+                exit(1);
+            }
+        } else {
+            /* Comment Extension */
+            /* Plain Text Extension */
+            /* Application Extension */
+        }
     } else {
     }
 
