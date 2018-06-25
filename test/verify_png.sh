bed2a33d6f995f53403b5f8a49a2ad13fb99e800
MIURA Yasuyuki <kokosabu@gmail.com>
2018-06-26 00:33:53 +0900

    add PngSuite

--- test/verify_png.sh
+++ test/verify_png.sh
@@ -6,16 +6,16 @@ files="./PngSuite-2017jul19/*.png"
 for filepath in $files; do
 
     ORIGINAL_FILE=$filepath
-    FILE1=${1/.png/_1.bmp}
-    FILE2=${1/.png/2.bmp}
-    FILE3=${1/.png/diff.bmp}
+    FILE1=${filepath/.png/_1.bmp}
+    FILE2=${filepath/.png/_2.bmp}
+    FILE3=${filepath/.png/_diff.bmp}
 
     echo $ORIGINAL_FILE
     # ループ
     ../src/my_image_decoder $ORIGINAL_FILE $FILE1 &> /dev/null
     if [ -e $FILE1 ]; then
         # 成功なら
-        convert $ORIGINAL_FILE $FILE2 &> /dev/null
+        convert $ORIGINAL_FILE bmp3:$FILE2 &> /dev/null
         composite -compose difference $FILE1 $FILE2 $FILE3 &> /dev/null
         RET=`identify -format "%[mean]" $FILE3`
         if [ $RET -eq 0 ]; then
