3f104d6075e0772e4c65535b285c9b251a79a119
MIURA Yasuyuki <kokosabu@gmail.com>
2018-06-25 23:54:23 +0900

    update verify_png.sh

--- test/verify_png.sh
+++ test/verify_png.sh
@@ -2,20 +2,28 @@
 
 # $#
 
-FILE=${1/png/bmp}
-FILE2=${1/.png/2.bmp}
+files="./PngSuite-2017jul19/*.png"
+for filepath in $files; do
 
-# ループ
-../src/my_image_decoder $1 $FILE &> /dev/null
-if [ -e $FILE ]; then
-    # 成功なら
-    convert $1 $FILE2 &> /dev/null
-    composite -compose difference $FILE $FILE2 diff.bmp &> /dev/null
-    RET=`identify -format "%[mean]" diff.bmp`
-    if [ $RET -eq 0 ]; then
-        echo "一致"
-    else
-        echo "不一致"
+    ORIGINAL_FILE=$filepath
+    FILE1=${1/.png/_1.bmp}
+    FILE2=${1/.png/2.bmp}
+    FILE3=${1/.png/diff.bmp}
+
+    echo $ORIGINAL_FILE
+    # ループ
+    ../src/my_image_decoder $ORIGINAL_FILE $FILE1 &> /dev/null
+    if [ -e $FILE1 ]; then
+        # 成功なら
+        convert $ORIGINAL_FILE $FILE2 &> /dev/null
+        composite -compose difference $FILE1 $FILE2 $FILE3 &> /dev/null
+        RET=`identify -format "%[mean]" $FILE3`
+        if [ $RET -eq 0 ]; then
+            echo "一致"
+        else
+            echo "不一致"
+        fi
     fi
-fi
-# -> 0なら一致
+    # -> 0なら一致
+
+done
