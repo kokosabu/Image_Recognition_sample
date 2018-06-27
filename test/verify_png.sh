b282a64e81f84fc10c7ddda44e3ff5e586e3b1a0
MIURA Yasuyuki <kokosabu@gmail.com>
2018-06-27 10:42:09 +0900

    rename variable

--- test/verify_png.sh
+++ test/verify_png.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
 
-# $#
-
 files="./PngSuite-2017jul19/*.png"
 for filepath in $files; do
 
     ORIGINAL_FILE=$filepath
-    FILE1=${filepath/.png/_1.bmp}
-    FILE2=${filepath/.png/_2.bmp}
-    FILE3=${filepath/.png/_diff.bmp}
+    MY_DECODE_FILE=${filepath/.png/_1.bmp}
+    IMAGEMAGICK_FILE=${filepath/.png/_2.bmp}
+    DIFF_FILE=${filepath/.png/_diff.bmp}
 
     echo $ORIGINAL_FILE
-    # ループ
-    ../src/my_image_decoder $ORIGINAL_FILE $FILE1 &> /dev/null
-    if [ -e $FILE1 ]; then
-        # 成功なら
-        convert $ORIGINAL_FILE bmp3:$FILE2 &> /dev/null
-        composite -compose difference $FILE1 $FILE2 $FILE3 &> /dev/null
-        RET=`identify -format "%[mean]" $FILE3`
+    ../src/my_image_decoder $ORIGINAL_FILE $MY_DECODE_FILE &> /dev/null
+    if [ -e $MY_DECODE_FILE ]; then
+        convert $ORIGINAL_FILE bmp3:$IMAGEMAGICK_FILE &> /dev/null
+        composite -compose difference $MY_DECODE_FILE $IMAGEMAGICK_FILE $DIFF_FILE &> /dev/null
+        RET=`identify -format "%[mean]" $DIFF_FILE`
         if [ $RET -eq 0 ]; then
             echo "一致"
         else
             echo "不一致"
         fi
+    else
+        echo "デコード失敗"
     fi
-    # -> 0なら一致
 
 done
